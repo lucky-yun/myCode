ce859046a6986d8d45e1f2247264c3f39553484a
qianfeng <qianfeng@qianfengdeMac-mini-7.local>
2015-11-23 15:09:01 -0800

    v3

---
+++ cvcv/cvcv/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cvcv
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright (c) 2015年 luoyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cvcv/cvcv/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cvcv
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright (c) 2015年 luoyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
